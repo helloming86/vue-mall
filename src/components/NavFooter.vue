074a5fa6112c34a1baf55aa3ea3e223d01967155
Nicky <helloming86@gmail.com>
2019-12-15 11:39:34 +0800

    Coding:首页Footer页面结构与渲染

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>Footer</div>
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="/imgs/logo-footer.png" alt="">
+      <p>小米商城</p>
+    </div>
+    <div class="footer-link">
+      <a href="https://google.com.hk" target="_blank">谷歌</a><span>|</span>
+      <a href="https://imooc.com" target="_blank">慕课网</a><span>|</span>
+      <a href="https://time.geekbang.org" target="_blank">极客时间</a>
+    </div>
+    <div class="copyright">CopyRight @2019 <span class="domain">mi.future.com</span> All Rights Reserved</div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +19,38 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  @import "~@/assets/scss/config.scss";
+  .footer{
+    height: 234px;
+    border-top: 4px solid $colorA;
+    background-color: $colorB ;
+    color: $colorD;
+    font-size: $fontI;
+    text-align: center;
+    .footer-logo{
+      margin-top: 46px;
+      margin-bottom: 31px;
+      img {
+        width: 53px;
+        height: 36px;
+        margin-bottom: 13px;
+      }
+    }
+    .footer-link{
+      a{
+        color: $colorD;
+        display: inline-block;
+      }
+      span{
+        margin: 0 10px;
+      }
+      margin-bottom: 13px;
+    }
+    .copyright{
+      .domain{
+        color: $colorA;
+      }
+    }
+  }
 </style>
