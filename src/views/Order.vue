c7fa8fec46cb69916cad98c86f102ce076bc91e8
Nicky <helloming86@gmail.com>
2020-01-15 14:02:49 +0800

    Coding:订单确认页面结构及渲染

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <order-header :title="title">
+      <template v-slot:tip>
+        <span>{{tip}}</span>
+      </template>
+    </order-header>
     <router-view/>
     <service-bar/>
     <order-footer/>
@@ -7,14 +12,35 @@
 </template>
 
 <script>
+import OrderHeader from 'components/OrderHeader'
 import ServiceBar from 'components/ServiceBar'
 import OrderFooter from '@/components/OrderFooter'
 
 export default {
   name: 'Order',
   components: {
+    OrderHeader,
     ServiceBar,
     OrderFooter
+  },
+  data () {
+    return {
+      title: '',
+      tip: ''
+    }
+  },
+  mounted () {
+    let path = this.$route.path
+    if (path === '/order/confirm') {
+      this.title = '订单确认'
+      this.tip = '请填写正切的收货地址信息'
+    } else if (path === '/order/list') {
+      this.title = '订单列表'
+      this.tip = '谨防钓鱼'
+    } else if (path === '/order/pay') {
+      this.title = '订单支付'
+      this.tip = '谨防钓鱼'
+    }
   }
 }
 </script>
